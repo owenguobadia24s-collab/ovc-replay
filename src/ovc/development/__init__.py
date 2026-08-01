515bf1d0f5dc7f14c5dd642233f99e3460769fb8
owenguobadia24s-collab <owenguobadia24s@gmail.com>
2026-08-01 16:44:04 +0100

    DA-WP2: expose preflight services

--- src/ovc/development/__init__.py
+++ src/ovc/development/__init__.py
@@ -8,6 +8,7 @@ from .artifacts import ArtifactRef, verify_artifact
 from .decisions import DecisionRecord
 from .gates import GatePacket
 from .identity import canonical_json_bytes, canonical_sha256, normalize_relative_path
+from .preflight import DestinationCheck, PreflightRequest, run_preflight
 from .profiles import ArtifactProfile, ProfileError, load_profile
 from .qa import QAAssertion, aggregate_assertions
 from .rollback import RollbackRecord
@@ -16,7 +17,9 @@ __all__ = [
     "ArtifactProfile",
     "ArtifactRef",
     "DecisionRecord",
+    "DestinationCheck",
     "GatePacket",
+    "PreflightRequest",
     "ProfileError",
     "QAAssertion",
     "RollbackRecord",
@@ -25,5 +28,6 @@ __all__ = [
     "canonical_sha256",
     "load_profile",
     "normalize_relative_path",
+    "run_preflight",
     "verify_artifact",
 ]
