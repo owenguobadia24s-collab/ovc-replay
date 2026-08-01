a27b92b325c47efb00de72c46d745e4b7fa096c2
owenguobadia24s-collab <owenguobadia24s@gmail.com>
2026-08-01 17:18:22 +0100

    DA-WP3: expose deterministic test selection services

--- src/ovc/development/__init__.py
+++ src/ovc/development/__init__.py
@@ -12,6 +12,12 @@ from .preflight import DestinationCheck, PreflightRequest, run_preflight
 from .profiles import ArtifactProfile, ProfileError, load_profile
 from .qa import QAAssertion, aggregate_assertions
 from .rollback import RollbackRecord
+from .test_selection import (
+    TestProfileRegistry,
+    TestSelectionError,
+    load_test_profile_registry,
+    select_test_manifest,
+)
 
 __all__ = [
     "ArtifactProfile",
@@ -23,11 +29,15 @@ __all__ = [
     "ProfileError",
     "QAAssertion",
     "RollbackRecord",
+    "TestProfileRegistry",
+    "TestSelectionError",
     "aggregate_assertions",
     "canonical_json_bytes",
     "canonical_sha256",
     "load_profile",
+    "load_test_profile_registry",
     "normalize_relative_path",
     "run_preflight",
+    "select_test_manifest",
     "verify_artifact",
 ]
