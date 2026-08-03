68b27b5b1481137307aeaee98fbd3841abf3b3c9
owenguobadia24s-collab <owenguobadia24s@gmail.com>
2026-08-03 20:37:46 +0100

    PCCR-G0: remove unapproved active namespace

--- src/ovc/research_operations/planned_closure_continuity/__init__.py
+++
@@ -1,3 +0,0 @@
-from .plan import PCCRPlanError, validate_fixture, validate_gate, validate_programme_state
-
-__all__ = ["PCCRPlanError", "validate_fixture", "validate_gate", "validate_programme_state"]
