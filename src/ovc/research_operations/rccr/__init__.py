b9f0125cec3bc173e05d9130fb0691aded67cc23
owenguobadia24s-collab <owenguobadia24s@gmail.com>
2026-08-15 20:37:05 +0100

    RCCRI-WP1: add RCCR package boundary

---
+++ src/ovc/research_operations/rccr/__init__.py
@@ -0,0 +1,15 @@
+from .core import (
+    RCCRAppendOnlyStore,
+    RCCRValidationError,
+    canonical_json_bytes,
+    logical_identity,
+    validate_canonical_object,
+)
+
+__all__ = [
+    "RCCRAppendOnlyStore",
+    "RCCRValidationError",
+    "canonical_json_bytes",
+    "logical_identity",
+    "validate_canonical_object",
+]
