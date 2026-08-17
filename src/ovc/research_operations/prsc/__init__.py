3102f3fcf5b5e1777510ba347bd72c8ddecbb8cb
owenguobadia24s-collab <owenguobadia24s@gmail.com>
2026-08-17 11:28:09 +0100

    PRSCI-WP2: reanchor public dependence/reference exports

--- src/ovc/research_operations/prsc/__init__.py
+++ src/ovc/research_operations/prsc/__init__.py
@@ -13,4 +13,27 @@ from .contracts import (
     build_protocol_generation,
     semantic_id,
 )
-__all__ = ["CHALLENGE_DIMENSIONS","PRSCContractError","adapt_ec1_record","build_protocol_generation","semantic_id"]
+from .dependence import (
+    DependenceGraphView,
+    adapt_evidence_dependence_graph,
+    build_candidate_dependence_profile,
+    build_inference_block_manifest,
+    leave_one_component_out,
+)
+from .reference import (
+    build_negative_space_controls,
+    build_reference_method_pack,
+    dependency_preserving_block_resample,
+    hac_ordered_secondary,
+    validate_reference_preservation,
+)
+
+__all__ = [
+    "CHALLENGE_DIMENSIONS", "PRSCContractError", "adapt_ec1_record",
+    "build_protocol_generation", "semantic_id", "DependenceGraphView",
+    "adapt_evidence_dependence_graph", "build_candidate_dependence_profile",
+    "build_inference_block_manifest", "leave_one_component_out",
+    "build_negative_space_controls", "build_reference_method_pack",
+    "dependency_preserving_block_resample", "hac_ordered_secondary",
+    "validate_reference_preservation",
+]
