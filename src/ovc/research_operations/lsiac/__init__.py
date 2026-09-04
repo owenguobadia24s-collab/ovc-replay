d002af246f4ed3327a30a661e9739e992672d3ca
owenguobadia24s-collab <owenguobadia24s@gmail.com>
2026-09-04 19:45:12 +0100

    LSIAC pass1: add research-operations package

---
+++ src/ovc/research_operations/lsiac/__init__.py
@@ -0,0 +1,13 @@
+"""Laboratory Scientific Inheritance and Accession support."""
+
+from .pass1 import (
+    build_pass1_classification_view,
+    build_shared_locator_dependence_graph,
+    load_source_passports,
+)
+
+__all__ = [
+    "build_pass1_classification_view",
+    "build_shared_locator_dependence_graph",
+    "load_source_passports",
+]
