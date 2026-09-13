18257842c69176bbc4180facc08ef83468eea47c
owenguobadia24s-collab <owenguobadia24s@gmail.com>
2026-09-13 14:37:07 +0100

    BCWT-R2: register bounded Research Operations namespace

--- tests/authority/test_active_namespace_allowlist.py
+++ tests/authority/test_active_namespace_allowlist.py
@@ -31,6 +31,7 @@ EXPECTED_OVC_PACKAGES = {
     "ovc.programme_genesis.grt_v0_2",
     "ovc.research_operations",
     "ovc.research_operations.asocs",
+    "ovc.research_operations.bcwt",
     "ovc.research_operations.cbs",
     "ovc.research_operations.cbs.comparators",
     "ovc.research_operations.c2_csm_reference",
