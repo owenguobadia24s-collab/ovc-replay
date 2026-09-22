57912b69391bb3267286e8990a48bbe599cf294f
owenguobadia24s-collab <owenguobadia24s@gmail.com>
2026-09-22 18:02:06 +0100

    PAER: add __init__.py

---
+++ src/ovc/research_operations/populations/__init__.py
@@ -0,0 +1,3 @@
+"""Central population allocation/exposure controls. Authority effect: NONE."""
+from .register import PopulationRegisterError, eligibility, population, record_allocation, record_exposure, validate_register
+__all__=["PopulationRegisterError","eligibility","population","record_allocation","record_exposure","validate_register"]
